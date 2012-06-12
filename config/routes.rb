55cae10b2d30c66681090cb9274371f86ccc45ee
Jawad Rehman <jrmemon@gmail.com>
2012-06-12 20:59:46 +0100

    finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ DemoApp::Application.routes.draw do
   get "static_pages/help"
   
   get "static_pages/about"
+  
+  get "static_pages/contact"
 
   resources :microposts
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,11 +9,17 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+    
   end
 
   describe "Help page" do
@@ -43,4 +49,17 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+    it "should have h1 'Contact page'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => 'Contact Us')
+    end
+  end
+
 end
